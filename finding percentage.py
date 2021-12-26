90c2196b6b07cd96cc7b7623248d88f81be5e201
Kavya-HM <85485341+Kavya-HM@users.noreply.github.com>
2021-12-26 23:30:58 +0530

    Create finding percentage.py

---
+++ finding percentage.py
@@ -0,0 +1,10 @@
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input()
+    x,y,z=student_marks[query_name]
+    print('%.2f'%((x+y+z)/3))
